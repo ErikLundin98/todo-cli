7a954937c413ce47c69624bc3a574ed8ff7d1cd7
Erik Lundin <lundin.erik@bcg.com>
2024-06-07 21:28:22 +0200

    Fix delete

--- todo/commands/remove.py
+++ todo/commands/remove.py
@@ -3,12 +3,16 @@
 from argparse import Namespace
 
 from todo import Session
-from todo.task import TaskORM
+from todo.task import SubTaskORM, TaskORM
 
 
 def handle_remove(args: Namespace):
     """Handle user using the remove command."""
     with Session() as session:
+        session.query(SubTaskORM).filter(
+            SubTaskORM.parent_id.isin(args.tasks)
+        ).delete()
+        
         session.query(TaskORM).filter(
             TaskORM.task_id.in_(args.tasks)
         ).delete()
